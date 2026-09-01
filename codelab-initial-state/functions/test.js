7c52c1b957db810cd76c09de9869707924b019dd
tjupp <travisjupp@gmail.com>
2026-09-01 06:41:44 -0600

    chore: relocate testing note comment to test file

--- codelab-initial-state/functions/test.js
+++ codelab-initial-state/functions/test.js
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// NOTE: FOR A PRODUCTION APP, ADD THESE TESTS TO OUR CONTINUOUS INTEGRATION PIPELINE.
 const fs = require("fs");
 const path = require("path");
 
